26d4fb1644c9737034db4af892179ee2b439abe7
sidoryak <sidoryak@gmail.com>
2022-07-22 10:30:47 +0700

    Implement operationApi and test

---
+++ src/main/java/com/example/calculator/web/OperationApi.java
@@ -0,0 +1,38 @@
+package com.example.calculator.web;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OperationApi {
+    @PostMapping(path="/operations")
+    public CalculateOperationResponse calculate (@RequestBody CalculateOperationRequest request) throws IllegalAccessException {
+        double result;
+
+        switch (request.operator()) {
+            case '+' -> result = request.x() + request.y();
+            case '-' -> result = request.x() - request.y();
+            case '*' -> result = request.x() * request.y();
+            case '/' -> result = request.x() / request.y();
+
+            default -> throw new IllegalAccessException("Unknown operator: %s ".formatted(request.operator()));
+        }
+
+        return new CalculateOperationResponse(result);
+
+    }
+
+
+
+}
+
+
+record CalculateOperationRequest (double x, double y, char operator)
+{
+
+}
+
+record CalculateOperationResponse(double result) {
+
+}
